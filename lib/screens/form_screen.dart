737e01359a957b111acacb65ef74af1f99f029c5
deaww77 <126137481+deaww77@users.noreply.github.com>
2025-06-14 14:58:44 +0700

    add lable to form

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -6,8 +6,28 @@ class FormScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("This is Page 2")),
-      body: Container(),
+      appBar: AppBar(title: Text("Data recording form")),
+      body: Form(
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: new InputDecoration(labelText: "ชื่อหนังสือ"),
+            ),
+            TextFormField(
+              decoration: new InputDecoration(labelText: "จำนวนเงิน"),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white, backgroundColor: Colors.purple, 
+              ),
+
+              onPressed: () {},
+
+              child: const Text('เพิ่มข้อมูล'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
